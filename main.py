ac0b6a90e6e9ffc491c186cde43c827b270229fc
Nick Schenone <nschenone16@gmail.com>
2023-08-18 22:01:53 +0000

    Updated main

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from src import create_and_set_project
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
     
-project = create_and_set_project(git_source="git://github.com/igz-us-sales/igz-liveops-demo#main")
+project = create_and_set_project(git_source="git://github.com/igz-us-sales/igz-liveops-demo#master")
 
 run_id = project.run(name="main", arguments=config, dirty=True, watch=True)
